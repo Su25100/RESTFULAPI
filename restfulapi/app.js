07c75a423cd38e7a61943a73e874db94ff9abcc9
su25100 <surbhibanke2599@gmail.com>
2022-07-03 09:14:05 +0530

    first commit

--- restfulapi/app.js
+++ restfulapi/app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
-
+app.get("/home", (req, res) => {
+    res.send("hello  developers!!");
+})
 app.post("/students", (req, res) => {
 
     res.send("hello guys!!");
 
 })
 
-app.listen()
+app.listen(port, () => {
+
+    console.log(`connection successfull!! at ${port}`);
+})
